b0128aca90ab9246a26eb156ef955f14dd4b47d4
Waqar Mohammad <waqar@waqar.co>
2018-12-24 23:43:17 +0000

    Added expenseType and wages pages along with saving data to db for these pages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 //////////////////// Connect DB ////////////////////
 
@@ -24,6 +25,8 @@ db.connect(err => {
 
 //initialise app
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //set view engine
 app.set('views', path.join(__dirname, 'views'));
@@ -33,8 +36,12 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req,res){
-    res.render('index', {
-        title : 'Sales Record',
+    let date = new Date();
+    let employees = 'SELECT * FROM employees';
+    let query = db.query(employees, (err, employees) => {
+        res.render('index', {
+        title: 'Sales Record',
+        date: date,
         days: [
             'Monday',
             'Tuesday',
@@ -43,15 +50,7 @@ app.get('/', function(req,res){
             'Friday',
             'Saturday'
         ],
-
-        employees: [
-            'emp1',
-            'emp2',
-            'emp3',
-            'emp4',
-            'emp5'
-        ],
-
+        employees: employees,
         expenses: [
             'expense 1',
             'expense 2',
@@ -67,12 +66,34 @@ app.get('/', function(req,res){
             'Expenses Total',
             'Net Profit'
         ]
+        });
     });
 });
 
+    
+        
+
+
+//         app.get('/dashboard', function (req, res) {
+//             let sql = 'SELECT * FROM passwords';
+//             let query = db.query(sql, (err, result) => {
+//                 res.render('dashboard', {
+//                     title: 'Password Manager',
+//                     result: result,
+//                     success: true,
+//                 });
+//             });
+//         });
+
+        
+
+       
+//     });
+// });
+
 app.get('/manage', function(req, res) {
     let sqlExpense = 'SELECT * FROM expenseType';
-    let sqlEmployee = "SELECT * FROM employees";
+    let sqlEmployee = 'SELECT * FROM employees';
 
     let query = db.query(sqlExpense, (err, result) => {
         if(err) throw err;
@@ -87,24 +108,84 @@ app.get('/manage', function(req, res) {
     });
 });
 
+//Add daily figures
+app.get('/add/daily', function(req, res){
+    res.render('add/daily', {
+        title: 'Add Daily Figures'
+    });
+});
 
-// app.get('/password/update/:id', function (req, res) {
-//     let id = req.params.id;
-//     let sql = `SELECT * FROM passwords WHERE id = '${id}'`;
+//Add daily to database
+app.post('/add/daily', function(req, res) {
+    let sql = 'INSERT INTO incomeDetail SET ?';
+    let daily = {
+        date: req.body.date,
+        cash: req.body.cash,
+        card: req.body.card,
+        cheque: req.body.cheque
+    }
 
-//     let query = db.query(sql, (err, result) => {
+    let query = db.query(sql, daily, (err, result) => {
+        if(err) throw err;
+        res.redirect('/');
+    });
+});
+
+
+// Add Employee Page
+app.get('/add/employee', function(req, res) {
+    res.render('add/employee', {
+        title: 'Add Employee'
+    });
+});
+
+//Add Employee to DB
+app.post('/add/employee', function(req, res) {
+    let sql = 'INSERT INTO employees SET ?';
+    let employee = {
+        name: req.body.name,
+        amount: req.body.amount
+    }
+    let query = db.query(sql, employee, (err, result) => {
+        if(err)throw err;
+        res.redirect('../manage')
+    });
+});
+
+//Add Expense Type Page
+app.get('/add/expense-type', function(req, res){
+    res.render('add/expense-type', {
+        title: 'Add Expense Type'
+    });
+});
+
+//Add Expense Type to DB
+app.post('/add/expense-type', function(req, res){
+    let sql = 'INSERT INTO expenseType SET ?';
+    let expense = {
+        name: req.body.expense
+    }
+
+    let query = db.query(sql, expense, (err, result) => {
+        if(err) throw err;
+        res.redirect('../manage');
+    });
+});
+
+
+// app.post('/password/add', function (req, res) {
+
+//     let sql = "INSERT INTO passwords SET ?";
+//     let password = {
+//         site: req.body.site,
+//         username: req.body.username,
+//         password: req.body.password
+//     }
+//     let query = db.query(sql, password, (err, result) => {
 //         if (err) throw err;
-//         console.log(result);
-//         res.render('update-password', {
-//             title: 'Update Password',
-//             site: result[0].site,
-//             username: result[0].username,
-//             password: result[0].password,
-//             id: id
-//         });
+//         res.redirect('../dashboard');
 //     });
-
-
+// });
 
 
 app.listen(3000, function(req, res){
